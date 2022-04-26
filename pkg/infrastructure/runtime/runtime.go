09eae6055179b541c4a7b92fa8d2caa832e33e20
Isak Styf <isak.styf@masarinconsulting.se>
2022-04-26 16:18:15 +0200

    add helper to extract source version from buildinfo

---
+++ pkg/infrastructure/runtime/runtime.go
@@ -0,0 +1,23 @@
+package runtime
+
+import "runtime/debug"
+
+func SourceVersion() string {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+
+	buildSettings := buildInfo.Settings
+	infoMap := map[string]string{}
+	for _, s := range buildSettings {
+		infoMap[s.Key] = s.Value
+	}
+
+	sha := infoMap["vcs.revision"]
+	if infoMap["vcs.modified"] == "true" {
+		sha += "+"
+	}
+
+	return sha
+}
