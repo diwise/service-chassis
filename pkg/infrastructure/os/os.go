edf63d9112a234ed6569064864c7bd24dd8bbf7e
Isak Styf <isak.styf@masarinconsulting.se>
2022-04-26 16:58:30 +0200

    add another env var helper

--- pkg/infrastructure/os/os.go
+++ pkg/infrastructure/os/os.go
@@ -6,6 +6,14 @@ import (
 	"github.com/rs/zerolog"
 )
 
+func GetEnvironmentVariableOrDefault(log zerolog.Logger, envVar, defaultValue string) string {
+	value := os.Getenv(envVar)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
 func GetEnvironmentVariableOrDie(log zerolog.Logger, envVar, description string) string {
 	value := os.Getenv(envVar)
 	if value == "" {
