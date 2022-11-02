67da00b28e7a5e0a21f27dac57632246c265743a
Isak Styf <isak.styf@masarinconsulting.se>
2022-11-02 15:14:00 +0100

    add service info to metrics export and increase frequency

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/diwise/service-chassis
 
-go 1.18
+go 1.19
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7
--- pkg/infrastructure/o11y/metrics/otel-metrics.go
+++ pkg/infrastructure/o11y/metrics/otel-metrics.go
@@ -3,11 +3,14 @@ package metrics
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/metric/global"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 type CleanupFunc func()
@@ -22,7 +25,17 @@ func Init(ctx context.Context, logger zerolog.Logger, serviceName, serviceVersio
 			return cleanupFunc, err
 		}
 
-		meterProvider := metric.NewMeterProvider(metric.WithReader(metric.NewPeriodicReader(exp)))
+		meterProvider := metric.NewMeterProvider(
+			metric.WithResource(resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(serviceName),
+				semconv.ServiceVersionKey.String(serviceVersion),
+			)),
+			metric.WithReader(
+				metric.NewPeriodicReader(exp, metric.WithInterval(10*time.Second)),
+			),
+		)
+
 		cleanupFunc = func() {
 			err := meterProvider.Shutdown(ctx)
 			if err != nil {
