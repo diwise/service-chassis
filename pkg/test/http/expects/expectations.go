f8b7a37fed3ffa9e57998f01980882f12b7c3712
Isak Styf <isak.styf@masarinconsulting.se>
2023-01-30 21:48:28 +0100

    add query param expectations

--- pkg/test/http/expects/expectations.go
+++ pkg/test/http/expects/expectations.go
@@ -49,3 +49,24 @@ func RequestPath(path string) func(*is.I, *http.Request) {
 		is.Equal(r.URL.Path, path)
 	}
 }
+
+func QueryParamContains(name, value string) func(*is.I, *http.Request) {
+	return func(is *is.I, r *http.Request) {
+		is.True(r.URL.Query().Has(name)) // query param should exist
+
+		for _, v := range strings.Split(r.URL.Query().Get(name), ",") {
+			if v == value {
+				return // it is a match!
+			}
+		}
+
+		is.Fail() // query params did not contain expected value
+	}
+}
+
+func QueryParamEquals(name, value string) func(*is.I, *http.Request) {
+	return func(is *is.I, r *http.Request) {
+		is.True(r.URL.Query().Has(name))         // query param should exist
+		is.Equal(r.URL.Query().Get(name), value) // query param should match
+	}
+}
