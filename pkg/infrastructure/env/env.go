ec7a1dd89d423e6f4ba55d7ead195b01d12c24a9
Mikael Nordberg <mikael.nordberg@outlook.com>
2026-05-29 10:54:40 +0200

    generic

--- pkg/infrastructure/env/env.go
+++ pkg/infrastructure/env/env.go
@@ -4,16 +4,36 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/diwise/service-chassis/pkg/infrastructure/o11y/logging"
 )
 
-func GetVariableOrDefault(ctx context.Context, envVar, defaultValue string) string {
+func GetVariableOrDefault[T string | int | bool](ctx context.Context, envVar string, defaultValue T) T {
+	var val T
 	value := os.Getenv(envVar)
 	if value == "" {
 		return defaultValue
 	}
-	return value
+
+	switch any(val).(type) {
+	case string:
+		return any(value).(T)
+	case int:
+		parsed, err := strconv.Atoi(value)
+		if err != nil {
+			return val
+		}
+		return any(parsed).(T)
+	case bool:
+		parsed, err := strconv.ParseBool(value)
+		if err != nil {
+			return val
+		}
+		return any(parsed).(T)
+	default:
+		panic(fmt.Sprintf("unsupported type: %T", val))
+	}
 }
 
 func GetVariableOrDie(ctx context.Context, envVar, description string) string {
