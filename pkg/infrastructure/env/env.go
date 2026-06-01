61d3bd55a1177dccb6d7edb6459f761438bca0de
Mikael Nordberg <mikael.nordberg@outlook.com>
2026-06-01 12:58:30 +0200

    change name

--- pkg/infrastructure/env/env.go
+++ pkg/infrastructure/env/env.go
@@ -10,66 +10,64 @@ import (
 	"github.com/diwise/service-chassis/pkg/infrastructure/o11y/logging"
 )
 
-func GetVariableOrDefault(ctx context.Context, envVar string, defaultValue string) string {
-	value := os.Getenv(envVar)
-	if value == "" {
-		return defaultValue
-	}
-	return value
-}
+var GetVariableOrDefault = GetVariableOrDefaultAs[string]
 
-func GetVariableOrDefaultT[T string | int | int32 | int64 | bool | time.Duration](ctx context.Context, envVar string, defaultValue T) T {
-	var val T
+var GetVariableOrDie = GetVariableOrDieAs[string]
+
+func GetVariableOrDefaultAs[T string | int | int32 | int64 | bool | time.Duration](ctx context.Context, envVar string, defaultValue T) T {
 	value := os.Getenv(envVar)
 	if value == "" {
 		return defaultValue
 	}
 
-	switch any(val).(type) {
-	case string:
-		return any(value).(T)
-	case int:
-		parsed, err := strconv.Atoi(value)
-		if err != nil {
-			return defaultValue
-		}
-		return any(parsed).(T)
-	case int32:
-		parsed, err := strconv.ParseInt(value, 10, 32)
-		if err != nil {
-			return defaultValue
-		}
-		return any(int32(parsed)).(T)
-	case int64:
-		parsed, err := strconv.ParseInt(value, 10, 64)
-		if err != nil {
-			return defaultValue
-		}
-		return any(parsed).(T)
-	case bool:
-		parsed, err := strconv.ParseBool(value)
-		if err != nil {
-			return defaultValue
-		}
-		return any(parsed).(T)
-	case time.Duration:
-		parsed, err := time.ParseDuration(value)
-		if err != nil {
-			return defaultValue
-		}
-		return any(parsed).(T)
-	default:
-		panic(fmt.Sprintf("unsupported type: %T", val))
+	parsed, err := parse[T](value)
+	if err != nil {
+		return defaultValue
 	}
+
+	return parsed
 }
 
-func GetVariableOrDie(ctx context.Context, envVar, description string) string {
+func GetVariableOrDieAs[T string | int | int32 | int64 | bool | time.Duration](ctx context.Context, envVar, description string) T {
 	value := os.Getenv(envVar)
+
 	if value == "" {
 		logger := logging.GetFromContext(ctx)
 		msg := fmt.Sprintf("please set %s to a valid %s.", envVar, description)
 		logger.Error(msg)
 		panic(msg)
 	}
-	return value
+
+	parsed, err := parse[T](value)
+	if err != nil {
+		panic(err)
+	}
+
+	return parsed
+}
+
+func parse[T string | int | int32 | int64 | bool | time.Duration](value string) (T, error) {
+	var val T
+
+	switch any(val).(type) {
+	case string:
+		return any(value).(T), nil
+	case int:
+		parsed, err := strconv.Atoi(value)
+		return any(parsed).(T), err
+	case int32:
+		parsed, err := strconv.ParseInt(value, 10, 32)
+		return any(int32(parsed)).(T), err
+	case int64:
+		parsed, err := strconv.ParseInt(value, 10, 64)
+		return any(parsed).(T), err
+	case bool:
+		parsed, err := strconv.ParseBool(value)
+		return any(parsed).(T), err
+	case time.Duration:
+		parsed, err := time.ParseDuration(value)
+		return any(parsed).(T), err
+	default:
+		panic(fmt.Sprintf("unsupported type: %T", val))
+	}
 }
--- pkg/infrastructure/env/env_test.go
+++ pkg/infrastructure/env/env_test.go
@@ -8,15 +8,15 @@ import (
 	"github.com/matryer/is"
 )
 
-func TestGetVariableOrDefault(t *testing.T) {
+func TestGetVariableOrDefaultAs(t *testing.T) {
 	ctx := context.Background()
 
 	t.Run("returns default for unset variables", func(t *testing.T) {
 		is := is.New(t)
 
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_UNSET_STRING", "default"), "default")
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_UNSET_INT", 42), 42)
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_UNSET_BOOL", true), true)
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_UNSET_STRING", "default"), "default")
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_UNSET_INT", 42), 42)
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_UNSET_BOOL", true), true)
 	})
 
 	t.Run("parses supported variable types", func(t *testing.T) {
@@ -25,9 +25,9 @@ func TestGetVariableOrDefault(t *testing.T) {
 		t.Setenv("SERVICE_CHASSIS_INT", "7")
 		t.Setenv("SERVICE_CHASSIS_BOOL", "false")
 
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_STRING", "default"), "configured")
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_INT", 42), 7)
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_BOOL", true), false)
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_STRING", "default"), "configured")
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_INT", 42), 7)
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_BOOL", true), false)
 	})
 
 	t.Run("returns default for invalid values", func(t *testing.T) {
@@ -35,7 +35,7 @@ func TestGetVariableOrDefault(t *testing.T) {
 		t.Setenv("SERVICE_CHASSIS_INVALID_INT", "seven")
 		t.Setenv("SERVICE_CHASSIS_INVALID_BOOL", "definitely")
 
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_INVALID_INT", 42), 42)
-		is.Equal(env.GetVariableOrDefault(ctx, "SERVICE_CHASSIS_INVALID_BOOL", true), true)
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_INVALID_INT", 42), 42)
+		is.Equal(env.GetVariableOrDefaultAs(ctx, "SERVICE_CHASSIS_INVALID_BOOL", true), true)
 	})
 }
